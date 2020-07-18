db7db93bd73b23923fc7b814c1d0caf7d9aa99b0
SwatiSSwain <SWAINSSWATI@GMAIL.COM>
2020-07-18 11:14:24 -0500

    SEtting up project

--- app.py
+++ app.py
@@ -1,9 +1,33 @@
-from flask import Flask
+from flask import Flask, jsonify, render_template
+import sqlalchemy
+import psycopg2
+from sqlalchemy import create_engine
+#from flask_sqlalchemy import SQLAlchemy
+import os
+#################################################
+# Flask Setup
+#################################################
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello World'
+#engine = create_engine(f'postgresql://swain:db@localhost:5432/Minneapolis_Police_Force_db')
+#connection = engine.connect()
+# Create a cursor object
+conn = psycopg2.connect(host="localhost", port = 5432, database="Minneapolis_Police_Force_db")
 
-if __name__ == '__main__':
-    app.run()
+
+cur = conn.cursor()
+# engine = psycopg2.connect("postgresql://postgres:postgres@localhost:52632/mydatabase", echo=False)
+#################################################
+# Flask Routes
+#################################################
+#pd.read_sql
+
+
+@app.route("/")
+def welcome():
+    cur.execute("select * from vw_police_use_of_force;")
+    nhbd = cur.fetchall()
+    return jsonify(nhbd)
+    #return "Hello World"
+if __name__ == "__main__":
+    app.run(debug=True)
