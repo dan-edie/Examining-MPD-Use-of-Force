7abdeb764fb79070c5edd7ddb64213589af6e8ab
SwatiSSwain <SWAINSSWATI@GMAIL.COM>
2020-07-18 13:02:29 -0500

    Adding if else to os.environ

--- app.py
+++ app.py
@@ -10,15 +10,31 @@ import os
 #################################################
 app = Flask(__name__)
 
+if "DATABASE_URL" in os.environ :
+    import urlparse # for python 3+ use: from urllib.parse import urlparse
+    result = urlparse.urlparse(os.environ.get("DATABASE_URL"))
+    # also in python 3+ use: urlparse("YourUrl") not urlparse.urlparse("YourUrl") 
+    username = result.username
+    password = result.password
+    database = result.path[1:]
+    hostname = result.hostname
+    conn = psycopg2.connect(
+    database = database,
+    user = username,
+    password = password,
+    host = hostname )
+else: 
+    conn = psycopg2.connect(host="localhost", port = 5432, database="Minneapolis_Police_Force_db")
+
+print(conn)
+
 #app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL") or 'postgres://swain:db@localhost/Minneapolis_Police_Force_db'
 #print(app.config['SQLALCHEMY_DATABASE_URI'])
 
 #engine = create_engine(f'postgresql://swain:db@localhost:5432/Minneapolis_Police_Force_db')
 #connection = engine.connect()
 # Create a cursor object
-conn = os.environ.get("DATABASE_URL") or psycopg2.connect(host="localhost", port = 5432, database="Minneapolis_Police_Force_db")
 
-print(conn)
 
 cur = conn.cursor()
 # engine = psycopg2.connect("postgresql://postgres:postgres@localhost:52632/mydatabase", echo=False)
